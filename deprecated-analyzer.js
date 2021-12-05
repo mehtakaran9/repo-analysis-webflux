6edb8b4ce908f68b666fd763d07aefcb7034ed06
Karan Mehta <mehtakaran9@gmail.com>
2021-12-05 10:52:35 -0800

    Updated Script for analyzing

--- deprecated-analyzer.js
+++ deprecated-analyzer.js
@@ -4,8 +4,6 @@ db.packages.find({},{ name:1,versions: 1 } ).forEach(function(npm){
     jsonRes["_id"]=npm.name;
     jsonRes["name"]=npm.name;
     var ver = npm.versions[Object.keys(npm.versions)[0]];
-    var project={};
-    print(Object.keys(npm.versions).length)
     jsonRes["versions"]=npm.versions;
         var ver = npm.versions[Object.keys(npm.versions)[Object.keys(npm.versions).length - 1]];
         if(ver["deprecated"]) {
